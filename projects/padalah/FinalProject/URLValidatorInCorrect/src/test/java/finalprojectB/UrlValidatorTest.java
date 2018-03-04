5c93313376895eda3eb5451a1e12e84b3b89479f
padalah <padalah@oregonstate.edu>
2018-03-03 19:14:44 -0800

    added tests for finalproject

--- projects/padalah/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/padalah/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -18,14 +18,58 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
-   public void testManualTest()
+   public void testManualTest01()
    {
-//You can use this function to implement your manual testing	   
-	   
+      //You can use this function to implement your manual testing
+      UrlValidator urlVal = new UrlValidator();
+      String value=null;
+      assertEquals(false,urlVal.isValid(value));
+
    }
-   
+
+   public void testManualTest02(){
+      UrlValidator urlVal = new UrlValidator();
+      String httpVal = "go.au:0/test1/#?action=view";
+      assertEquals(false, urlVal.isValid(httpVal));
+   }
+
+   public void testManualTest03(){
+      UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      String httpVal = "http://go:.com/test1?action=view";
+      assertEquals(true, urlVal.isValid(httpVal));
+   }
+
+   public void testManualTest04(){
+      UrlValidator urlVal = new UrlValidator();
+      String h3tVal = "h3t://aaa./test15//action=view";
+      assertEquals(false, urlVal.isValid(h3tVal));
+
+   }
+
+   public void testManualTest05(){
+      UrlValidator urlVal = new UrlValidator();
+      String h3tVal = "3ht://www.google.com/?action=view";
+      assertEquals(false, urlVal.isValid(h3tVal));
+
+   }
+
+   public void testManualTest06(){
+      UrlValidator urlVal = new UrlValidator();
+      String h3tVal = "ftp://256.256.256.256/test15/action=view";
+      assertEquals(false, urlVal.isValid(h3tVal));
+   }
+
+   public void testManualTest07(){
+      UrlValidator urlVal = new UrlValidator();
+      String ftpVal = "ftp://255.255.255.255/test1?action=view";
+      assertEquals(true, urlVal.isValid(ftpVal));
+   }
+
+   public void testManualTest08(){
+      UrlValidator urlVal = new UrlValidator();
+      String ftpVal = "ftp://www.google.com/";
+      assertEquals(true, urlVal.isValid(ftpVal));
+   }
    
    public void testYourFirstPartition()
    {
