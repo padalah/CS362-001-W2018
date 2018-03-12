c8a76fb18ee28c76f84a9eccee9cb711bcf6c8aa
padalah <padalah@oregonstate.edu>
2018-03-12 16:29:54 -0700

    Final Project Invalidator B test

--- projects/padalah/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/padalah/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -3,6 +3,8 @@ package finalprojectB;
 
 import junit.framework.TestCase;
 
+import java.util.Random;
+
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
@@ -104,9 +106,233 @@ public class UrlValidatorTest extends TestCase {
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
+      UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      for(int i=0; i<50; i++) {
+         StringBuilder urlBuffer = new StringBuilder();
+         Random rand = new Random();
+         int urlSchemeTNum = rand.nextInt(testUrlSchemeTrue.length);
+         int urlAuthorityTNum = rand.nextInt(testUrlAuthorityTrue.length);
+         int urlPortTNum = rand.nextInt(testUrlPortTrue.length);
+         int urlPathTNum = rand.nextInt(testPathTrue.length);
+         int urlPathOptionsTNum = rand.nextInt(testUrlPathOptionsTrue.length);
+         int urlQueryTNum = rand.nextInt(testUrlQueryTrue.length);
+         int numOptionTimes = rand.nextInt(6);
+         for(int j=0; j<numOptionTimes; j++){
+            if(j==0){
+               urlBuffer.append(testUrlSchemeTrue[urlSchemeTNum]);
+            }
+            else if(j==1){
+               urlBuffer.append(testUrlAuthorityTrue[urlAuthorityTNum]);
+            }
+            else if(j==2){
+               urlBuffer.append(testUrlPortTrue[urlPortTNum]);
+            }
+            else if(j==3){
+               urlBuffer.append(testPathTrue[urlPathTNum]);
+            }
+            else if(j==4){
+               urlBuffer.append(testUrlPathOptionsTrue[urlPathOptionsTNum]);
+            }
+            else if(j==5){
+               urlBuffer.append(testUrlQueryTrue[urlQueryTNum]);
+            }
+         }
+         String url = urlBuffer.toString();
+         assertTrue(urlVal.isValid(url));
+      }
 
+      for(int i=0; i<50; i++) {
+         StringBuilder urlBuffer = new StringBuilder();
+         Random rand = new Random();
+         int urlSchemeFNum = rand.nextInt(testUrlSchemeFalse.length);
+         int urlAuthorityFNum = rand.nextInt(testUrlAuthorityFalse.length);
+         int urlPortFNum = rand.nextInt(testUrlPortFalse.length);
+         int urlPathFNum = rand.nextInt(testPathFalse.length);
+         int urlPathOptionsFNum = rand.nextInt(testUrlPathOptionsFalse.length);
+         int urlQueryFNum = rand.nextInt(testUrlQueryFalse.length);
+         int numOptionTimes = rand.nextInt(6);
+         for(int j=0; j<numOptionTimes; j++){
+            if(j==0){
+               urlBuffer.append(testUrlSchemeFalse[urlSchemeFNum]);
+            }
+            else if(j==1){
+               urlBuffer.append(testUrlAuthorityFalse[urlAuthorityFNum]);
+            }
+            else if(j==2){
+               urlBuffer.append(testUrlPortFalse[urlPortFNum]);
+            }
+            else if(j==3){
+               urlBuffer.append(testPathFalse[urlPathFNum]);
+            }
+            else if(j==4){
+               urlBuffer.append(testUrlPathOptionsFalse[urlPathOptionsFNum]);
+            }
+            else if(j==5){
+               urlBuffer.append(testUrlQueryFalse[urlQueryFNum]);
+            }
+         }
+         String url = urlBuffer.toString();
+         assertFalse(urlVal.isValid(url));
+      }
+
+      for(int i=0; i<50; i++) {
+         StringBuilder urlBuffer = new StringBuilder();
+         Random rand = new Random();
+         int urlSchemeTNum = rand.nextInt(testUrlSchemeTrue.length);
+         int urlAuthorityTNum = rand.nextInt(testUrlAuthorityTrue.length);
+         int urlPortTNum = rand.nextInt(testUrlPortTrue.length);
+         int urlPathTNum = rand.nextInt(testPathTrue.length);
+         int urlPathOptionsTNum = rand.nextInt(testUrlPathOptionsTrue.length);
+         int urlQueryTNum = rand.nextInt(testUrlQueryTrue.length);
+         int urlSchemeFNum = rand.nextInt(testUrlSchemeFalse.length);
+         int urlAuthorityFNum = rand.nextInt(testUrlAuthorityFalse.length);
+         int urlPortFNum = rand.nextInt(testUrlPortFalse.length);
+         int urlPathFNum = rand.nextInt(testPathFalse.length);
+         int urlPathOptionsFNum = rand.nextInt(testUrlPathOptionsFalse.length);
+         int urlQueryFNum = rand.nextInt(testUrlQueryFalse.length);
+         int numOptionTimes = rand.nextInt(6);
+         final int one=1;
+         Boolean trueorfalse = false;
+         for(int j=0; j<numOptionTimes; j++){
+            int choice = rand.nextInt(1);
+            while(j<one) {
+               if (choice == 1) {
+                  trueorfalse = true;
+               }
+            }
+            if(j==0 && choice==0){
+               urlBuffer.append(testUrlSchemeTrue[urlSchemeTNum]);
+            }
+            else if(j==0 && choice==1){
+               urlBuffer.append(testUrlSchemeFalse[urlSchemeFNum]);
+            }
+            else if(j==1 && choice==0){
+               urlBuffer.append(testUrlAuthorityTrue[urlAuthorityTNum]);
+            }
+            else if(j==1 && choice==1){
+               urlBuffer.append(testUrlAuthorityFalse[urlAuthorityFNum]);
+            }
+            else if(j==2 && choice==0){
+               urlBuffer.append(testUrlPortTrue[urlPortTNum]);
+            }
+            else if(j==2 && choice==1){
+               urlBuffer.append(testUrlPortFalse[urlPortFNum]);
+            }
+            else if(j==3 && choice==0){
+               urlBuffer.append(testPathTrue[urlPathTNum]);
+            }
+            else if(j==3 && choice==1){
+               urlBuffer.append(testPathFalse[urlPathFNum]);
+            }
+            else if(j==4 && choice==0){
+               urlBuffer.append(testUrlPathOptionsTrue[urlPathOptionsTNum]);
+            }
+            else if(j==4 && choice==1){
+               urlBuffer.append(testUrlPathOptionsFalse[urlPathOptionsFNum]);
+            }
+            else if(j==5 && choice==0){
+               urlBuffer.append(testUrlQueryTrue[urlQueryTNum]);
+            }
+            else if(j==5 && choice==1){
+               urlBuffer.append(testUrlQueryFalse[urlQueryFNum]);
+            }
+         }
+         String url = urlBuffer.toString();
+         if(trueorfalse==true) {
+            assertFalse(urlVal.isValid(url));
+         }
+         else{
+            assertTrue(urlVal.isValid(url));
+         }
+      }
    }
-   
 
+
+   String[] testUrlSchemeTrue = {"http://", "ftp://", "h3t://", ""};
+
+   String[] testUrlSchemeFalse = {"3ht://", "http:/", "http:", "http/", "://"};
+
+   String[] testUrlAuthorityTrue = {
+           "www.google.com",
+           "go.com",
+           "go.au",
+           "0.0.0.0",
+           "255.255.255.255",
+           "255.com"
+   };
+
+   String[] testUrlAuthorityFalse = {
+           "256.256.256.256",
+           "1.2.3.4.5",
+           "1.2.3.4.",
+           "1.2.3",
+           ".1.2.3.4",
+           "go.a",
+           "go.a1a",
+           "go.1aa",
+           "aaa.",
+           ".aaa",
+           "aaa",
+           ""
+   };
+
+   String[] testUrlPortTrue = {
+           ":80",
+           ":65535",
+           ":0",
+           ""
+   };
+
+   String[] testUrlPortFalse = {
+           ":-1",
+           ":65636",
+           ":65a"
+   };
+
+   String[] testPathTrue = {"/test1",
+           "/t123",
+           "/$23",
+           "/test1/",
+           "",
+           "/test1/file"
+   };
+
+   String[] testPathFalse = {
+           "/..",
+           "/../",
+           "/..//file",
+           "/test1//file"
+   };
+
+   //Test allow2slash, noFragment
+   String[] testUrlPathOptionsTrue = {"/test1",
+           "/t123",
+           "/$23",
+           "/test1/",
+           "",
+           "/test1/file",
+           "/t123/file",
+           "/$23/file",
+           "/test1//file"
+   };
+
+   String[] testUrlPathOptionsFalse = {
+           "/..",
+           "/../",
+           "/#",
+           "/../file",
+           "/..//file",
+           "/#/file"
+   };
+
+   String[] testUrlQueryTrue = {"?action=view",
+           "?action=edit&mode=up",
+           ""
+   };
+
+   String[] testUrlQueryFalse = {"?action?=view",
+           "?action=//edit&mode=up",
+           "&%$@#"
+   };
 
 }
